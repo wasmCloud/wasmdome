2ab1aa812087d5a93f12da9ba3fe04142b0d8fa2
autodidaddict <alothien@gmail.com>
2020-05-14 07:53:49 -0400

    First attempt at signing all release wasm modules

--- keyprep.sh
+++ keyprep.sh
@@ -4,3 +4,16 @@
 
 mkdir -p ./command-processor/.keys && echo "$WASMDOME_ACCOUNT_KEY" > ./command-processor/.keys/account.nk
 echo "$CMDPROC_MODULE_KEY" > ./command-processor/.keys/module.nk
+
+mkdir -p ./historian/.keys && echo "$WASMDOME_ACCOUNT_KEY" > ./historian/.keys/account.nk
+echo "$HISTORIAN_MODULE_KEY" > ./historian/.keys/module.nk
+
+mkdir -p ./leaderboard/.keys && echo "$WASMDOME_ACCOUNT_KEY" > ./leaderboard/.keys/account.nk
+echo "$LEADERBOARD_MODULE_KEY" > ./leaderboard/.keys/module.nk 
+
+mkdir -p ./match-coord/.keys && echo "$WASMDOME_ACCOUNT_KEY" > ./match-coord/.keys/account.nk
+echo "$MATCHCOORD_MODULE_KEY" > ./match-coord/.keys/module.nk
+
+# Official NPCs
+mkdir -p ./NPC/turret/.keys && echo "$WASMDOME_ACCOUNT_KEY" > ./NPC/turret/.keys/account.nk
+echo "$TURRET1_MODULE_KEY" > ./NPC/turret/.keys/module.nk 
