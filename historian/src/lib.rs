0dbd0f669e8f421f298564a8730c86912c126de6
Kevin Hoffman <alothien@gmail.com>
2020-06-22 09:03:21 -0400

    historian should compile now (though it may need more feature work later)

--- historian/src/lib.rs
+++ historian/src/lib.rs
@@ -1,6 +1,7 @@
+#[macro_use]
+extern crate log;
 extern crate wascc_actor as actor;
 extern crate wasmdome_protocol as protocol;
-extern crate log;
 
 use protocol::events::*;
 
@@ -8,10 +9,12 @@ const SUBJECT_TRIGGER_REPLAY: &str = "wasmdome.history.replay";
 
 use actor::events::EventStreamsHostBinding;
 use actor::prelude::*;
-use log::info;
 use std::collections::HashMap;
 
-actor_handlers! { codec::messaging::OP_DELIVER_MESSAGE => handle_message, codec::core::OP_HEALTH_REQUEST => health }
+actor_handlers! {
+    codec::messaging::OP_DELIVER_MESSAGE => handle_message,
+    codec::core::OP_HEALTH_REQUEST => health
+}
 
 fn health(_req: codec::core::HealthRequest) -> HandlerResult<()> {
     Ok(())
@@ -23,53 +26,57 @@ fn handle_message(msg: codec::messaging::BrokerMessage) -> HandlerResult<()> {
     let events = events::default();
     if msg.subject == SUBJECT_TRIGGER_REPLAY {
         trigger_replay(events, msg.body)
-    } else if msg.subject.starts_with(SUBJECT_MATCH_EVENTS_PREFIX) {
+    } else if is_match_event_subject(&msg.subject) {
+        //TODO? does not currently record arena events like actor up/down and match start/complete
         record_match_event(events, msg.body)
     } else {
         Ok(())
     }
 }
 
-fn record_match_event(
-    events: EventStreamsHostBinding,
-    msg: Vec<u8>,
-) -> HandlerResult<()> {
+/// Returns the stream ID for a match as persisted in the event stream provider
+/// NOT to be confused with a message broker subject for a match
+fn match_stream_id(match_id: &str) -> String {
+    format!("wasmdome.history.match.{}", match_id)
+}
+
+fn is_match_event_subject(subject: &str) -> bool {
+    subject.to_lowercase().starts_with("wasmdome.match.")
+        && subject.to_lowercase().ends_with(".events")
+}
+
+/// Returns the subject on which MatchEvents will be replayed
+fn replay_subject(match_id: &str) -> String {
+    format!("wasmdome.match.{}.events.replay", match_id) // live subject is `wasmdome.match.{}.events`
+}
+
+fn record_match_event(events: EventStreamsHostBinding, msg: Vec<u8>) -> HandlerResult<()> {
     let evt: MatchEvent = serde_json::from_slice(&msg)?;
-    info!("Recording match event: {:?}", evt);
+    trace!("Recording match event: {:?}", evt);
     let mut hash = HashMap::new();
     hash.insert("json".to_string(), serde_json::to_string(&evt)?);
 
-    let _id = events.write_event(
-        &format!("wasmdome.history.match.{}", extract_match_id(&evt)),
-        hash,
-    )?;
+    let _id = events.write_event(&match_stream_id(&extract_match_id(&evt)), hash)?;
 
     Ok(())
 }
 
 fn extract_match_id(evt: &MatchEvent) -> String {
     match evt {
-        MatchEvent::MatchCreated { match_id, .. } => match_id.to_string(),
-        MatchEvent::ActorStarted { match_id, .. } => match_id.to_string(),
-        MatchEvent::MatchStarted { match_id, .. } => match_id.to_string(),
-        MatchEvent::TurnRequested { match_id, .. } => match_id.to_string(),
         MatchEvent::TurnEvent { match_id, .. } => match_id.to_string(),
     }
 }
 
-fn trigger_replay(
-    events: EventStreamsHostBinding,
-    msg: Vec<u8>,
-) -> HandlerResult<()> {
+fn trigger_replay(events: EventStreamsHostBinding, msg: Vec<u8>) -> HandlerResult<()> {
     let trigger: serde_json::Value = serde_json::from_slice(&msg)?;
     let match_id = trigger["match_id"].as_str().unwrap().to_string();
-    info!("Triggering replay of match {}", match_id);
+    trace!("Triggering replay of match {}", match_id);
     replay(events, &match_id)
 }
 
 fn replay(events: EventStreamsHostBinding, match_id: &str) -> HandlerResult<()> {
-    let evts = events.read_all(&format!("wasmdome.history.match.{}", match_id))?;
-    let replay_subject = format!("wasmdome.match_events.{}.replay", match_id);
+    let evts = events.read_all(&match_stream_id(match_id))?;
+    let replay_subject = replay_subject(match_id);
     for event in evts {
         messaging::default().publish(&replay_subject, None, event.values["json"].as_bytes())?;
     }
