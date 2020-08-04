1f23644fab112fc936aa3d64f354fa7e9bbcdd63
Brooks Townsend <brooks.townsend@capitalone.com>
2020-08-04 14:20:56 -0400

    compete command

--- wasmdome/src/main.rs
+++ wasmdome/src/main.rs
@@ -5,7 +5,8 @@ use crossbeam_channel::unbounded;
 use protocol::commands::{ArenaControlCommand::*, CreateMatch, MechQueryResponse};
 use protocol::events::ArenaEvent;
 use protocol::scheduler::StoredMatch;
-use std::{error::Error, path::PathBuf};
+use protocol::tools::{CredentialsRequest, CredentialsResponse};
+use std::{error::Error, path::PathBuf, time::Duration};
 use structopt::clap::AppSettings;
 use structopt::StructOpt;
 use uuid::Uuid;
@@ -36,6 +37,14 @@ struct CliCommand {
 
 #[derive(Debug, Clone, StructOpt)]
 enum WasmdomeAction {
+    /// Configure local credentials to compete in the arena by submitting an access token
+    Compete {
+        /// Your account public key
+        account: String,
+
+        /// Short-lived access token granted by the wasmdome.dev website
+        token: String,
+    },
     /// Query the schedule of upcoming matches
     Schedule,
     /// Run a wasmdome match using the local lattice
@@ -63,8 +72,64 @@ fn handle_command(cmd: CliCommand) -> std::result::Result<(), Box<dyn ::std::err
             board_height,
             board_width,
         } => run_match(nc, max_turns, board_height, board_width)?,
+        WasmdomeAction::Compete { token, account } => change_compete_creds(nc, &account, &token)?,
     };
     Ok(())
+}
+
+fn change_compete_creds(
+    nc: nats::Connection,
+    account: &str,
+    token: &str,
+) -> Result<(), Box<dyn Error>> {
+    let req = CredentialsRequest {
+        account_key: account.to_string(),
+        token: token.to_string(),
+    };
+    let res = nc.request_timeout(
+        "wasmdome.public.creds.claim",
+        &serde_json::to_vec(&req)?,
+        Duration::from_millis(500),
+    )?;
+    write_arena_creds(serde_json::from_slice(&res.data)?)?;
+    Ok(())
+}
+
+fn write_arena_creds(creds: CredentialsResponse) -> Result<(), Box<dyn Error>> {
+    println!("{:?}", creds);
+    let homedir = dirs::home_dir();
+    if homedir.is_none() {
+        return Err("Cannot locate home directory".into());
+    }
+    let domedir = homedir.unwrap().join(".wasmdome/");
+    let _ = std::fs::create_dir_all(&domedir)?;
+    if let CredentialsResponse::Valid {
+        user_jwt,
+        user_secret,
+    } = creds
+    {
+        let fcontents = format!(
+            r#"
+-----BEGIN NATS USER JWT-----
+{}
+------END NATS USER JWT------
+************************* IMPORTANT *************************
+NKEY Seed printed below can be used to sign and prove identity.
+NKEYs are sensitive and should be treated as secrets.
+-----BEGIN USER NKEY SEED-----
+{}
+------END USER NKEY SEED------
+*************************************************************
+"#,
+            user_jwt, user_secret
+        );
+        let domefile = domedir.join("arena.creds");
+        std::fs::write(domefile, fcontents.as_bytes())?;
+        println!("New arena credentials written to ~/.wasmdome/arena.creds");
+        Ok(())
+    } else {
+        Err("Did not obtain valid arena credentials".into())
+    }
 }
 
 fn check_schedule(nc: nats::Connection) -> Result<(), Box<dyn Error>> {
