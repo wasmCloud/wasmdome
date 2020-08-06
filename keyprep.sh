36f9528919c2c94ef95533401f25e49cd739009b
Kevin Hoffman <alothien@gmail.com>
2020-08-06 10:14:51 -0400

    Fixing keyprep script

--- keyprep.sh
+++ keyprep.sh
@@ -11,3 +11,14 @@ echo "$TURRET2_MODULE_KEY" > ./NPC/corner-turret/.keys/module.nk
 
 mkdir -p ./NPC/kode-frieze/.keys && echo "$WASMDOME_ACCOUNT_KEY" > ./NPC/kode-frieze/.keys/account.nk
 echo "$KODEFRIEZE_MODULE_KEY" > ./NPC/kode-frieze/.keys/module.nk 
+
+mkdir -p ./NPC/boylur-plait/.keys && echo "$WASMDOME_ACCOUNT_KEY" > ./NPC/boylur-plait/.keys/account.nk
+echo "$BOYLUR_MODULE_KEY" > ./NPC/boylur-plait/.keys/module.nk 
+
+mkdir -p ./NPC/sir-emony/.keys && echo "$WASMDOME_ACCOUNT_KEY" > ./NPC/sir-emony/.keys/account.nk
+echo "$SIREMONY_MODULE_KEY" > ./NPC/sir-emony/.keys/module.nk 
+
+mkdir -p ./NPC/deploy-jenkins/.keys && echo "$WASMDOME_ACCOUNT_KEY" > ./NPC/deploy-jenkins/.keys/account.nk
+echo "$DEPLOY_MODULE_KEY" > ./NPC/deploy-jenkins/.keys/module.nk 
+
+
